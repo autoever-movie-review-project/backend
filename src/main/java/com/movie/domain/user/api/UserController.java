874640aa66cc075cddf6344137fb4e2af3972d60
joo9837 <joo9837@naver.com>
2024-11-09 22:56:23 +0900

    feat(User/api): UserController 초기 세팅
    - UserController

--- src/main/java/com/movie/domain/user/api/UserController.java
+++ src/main/java/com/movie/domain/user/api/UserController.java
@@ -1,4 +1,15 @@
 package com.movie.domain.user.api;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "user", description = "user domain apis")
+@Slf4j
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
 public class UserController {
 }
