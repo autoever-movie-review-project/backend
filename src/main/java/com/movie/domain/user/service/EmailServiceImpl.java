8afff0a90b20e36e5ad2eb5a11c2cacc1d4be852
joo9837 <joo9837@naver.com>
2024-11-10 02:20:23 +0900

    Style(User/service): 주석 추가

--- src/main/java/com/movie/domain/user/service/EmailServiceImpl.java
+++ src/main/java/com/movie/domain/user/service/EmailServiceImpl.java
@@ -1,38 +1,44 @@
 package com.movie.domain.user.service;
 
-import com.movie.domain.user.dao.LogoutAccessTokenRedisRepository;
-import com.movie.domain.user.dao.RefreshTokenRedisRepository;
+import com.movie.domain.user.constant.EmailExceptionMessage;
 import com.movie.domain.user.dao.UserRepository;
 import com.movie.domain.user.dto.request.CheckEmailCodeReqDto;
 import com.movie.domain.user.dto.request.CheckEmailReqDto;
 import com.movie.domain.user.dto.response.CheckResDto;
+import com.movie.domain.user.exception.EmailCodeNotFoundException;
 import com.movie.domain.user.exception.EmailDuplicatedException;
-import com.movie.global.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-import static com.movie.domain.user.constant.UserExceptionMessage.EMAIL_DUPLICATED;
+import static com.movie.domain.user.constant.EmailExceptionMessage.EMAIL_DUPLICATED;
 
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
 
     private final UserRepository userRepository;
-    private final RefreshTokenRedisRepository refreshTokenRepository;
-    private final LogoutAccessTokenRedisRepository logoutAccessTokenRepository;
     private final RedisTemplate<String, Object> redisTemplate;
     private final JavaMailSender javaMailSender;
     private Random random;
 
+    /**
+     * 인증번호 및 임시 비밀번호 생성
+     * 랜덤으로 코드 생성
+     */
+    @Transactional
     @Override
     public void sendEmailCode(CheckEmailReqDto checkEmailReqDto) {
         // 임의의 authKey 생성
@@ -45,42 +51,61 @@ public class EmailServiceImpl implements EmailService {
         try {
             MimeMessage mimeMessage = javaMailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "utf-8");
-            helper.setTo(checkEmailReqDto.getEmail());
-            helper.setSubject(subject);
+            helper.setTo(checkEmailReqDto.getEmail()); //메일 수신자 설정
+            helper.setSubject(subject); // 메일 제목 설정
             helper.setText(text, true); // HTML이라는 의미로 true.
+
+            log.info("[이메일 발신] 이메일 작성 완료. 발송 시도");
             javaMailSender.send(mimeMessage);
         } catch (MessagingException e) {
+            log.error("[이메일 발신] 발신 실패.");
             e.printStackTrace();
         }
         ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
 
         // 유효 시간(5분)동안 {email, authKey} 저장
         valueOperations.set(checkEmailReqDto.getEmail(), authKey, 60 * 5L, TimeUnit.SECONDS);
+
+        log.info("[이메일 발신] 이메일 발신 성공. email : {}, code : {}", checkEmailReqDto.getEmail(), authKey);
     }
 
+    /**
+     * 회원가입 전 이메일 중복검사
+     */
+    @Transactional
     @Override
     public CheckResDto checkEmailDuplicated(CheckEmailReqDto checkEmailReqDto) {
+        log.info("[이메일 중복 검사] 중복 검사 요청. email : {}", checkEmailReqDto.getEmail());
         if (userRepository.findByEmail(checkEmailReqDto.getEmail()).isPresent()) {
+            log.error("[이메일 중복 검사] 이메일 중복.");
             throw new EmailDuplicatedException(EMAIL_DUPLICATED.getMessage());
         }
+        log.info("[이메일 중복 검사] 중복 검사 완료.");
         return CheckResDto.builder()
                 .success(true)
                 .build();
     }
 
+    /**
+     * 발송된 이메일 인증 코드와, 작성한 인증 코드 일치 체크
+     */
+    @Transactional
     @Override
     public CheckResDto checkEmailCode(CheckEmailCodeReqDto checkEmailCodeReqDto) {
+        log.info("[이메일 인증 코드 유효검사] 검사 요청. email : {}, code : {}", checkEmailCodeReqDto.getEmail(), checkEmailCodeReqDto.getCode());
         ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
         String originCode = (String) valueOperations.get(checkEmailCodeReqDto.getEmail());
         if (originCode == null)
-            throw new NotFoundException("해당 이메일로 유효한 인증 코드가 존재하지 않습니다");
+            throw new EmailCodeNotFoundException(EmailExceptionMessage.EMAIL_CODE_NOT_FOUND.getMessage());
         Boolean result = false;
         if (originCode.equals(checkEmailCodeReqDto.getCode())) {
             result = true;
-            // 코드를 확인했으므로 redis 에서 삭제
+            // 임시 코드 redis 에서 삭제
             valueOperations.getOperations().delete(checkEmailCodeReqDto.getEmail());
-            valueOperations.set(checkEmailCodeReqDto.getEmail(), "이메일 인증 완료", 60 * 5L, TimeUnit.SECONDS);
+            // 인증 완료 저장
+            valueOperations.set(checkEmailCodeReqDto.getEmail(), "verified", 60 * 5L, TimeUnit.SECONDS);
         }
+
         return CheckResDto.builder()
                 // 존재하면 false, 존재하지 않으면 true 반환
                 .success(result)
