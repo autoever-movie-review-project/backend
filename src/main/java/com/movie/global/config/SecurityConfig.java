097bb4fc6471be52e4349c998d8248c0b8463f53
joohee <joo9837@naver.com>
2024-11-13 09:52:30 +0900

    Feat(Global/config): CORS 설정에 Authorization 헤더 노출 추가

--- src/main/java/com/movie/global/config/SecurityConfig.java
+++ src/main/java/com/movie/global/config/SecurityConfig.java
@@ -76,6 +76,7 @@ public class SecurityConfig {
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
+        configuration.addExposedHeader("Authorization");
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 
